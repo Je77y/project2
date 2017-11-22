57516ac3aa55672eb4139a544d698e127140b87c
4un9cu0ng <3h08mi@gmail.com>
2017-11-22 17:48:27 +0700

    Giao dien Admin

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix'=>'admin'], function(){
+	Route::group(['prefix'=>'theloai'], function(){
+		Route::get('danhsach', 'TheLoaiController@getDanhSach');
+		Route::get('them', 'TheLoaiController@getThem');
+		Route::get('sua', 'TheLoaiController@getSua');
+	});
+
+	Route::group(['prefix'=>'loaitin'], function(){
+		Route::get('danhsach', 'LoaiTinController@getDanhSach');
+		Route::get('them', 'LoaiTinController@getThem');
+		Route::get('sua', 'LoaiTinController@getSua');
+	});
+
+	Route::group(['prefix'=>'tintuc'], function(){
+		Route::get('danhsach', 'TinTucController@getDanhSach');
+		Route::get('them', 'TinTucController@getThem');
+		Route::get('sua', 'TinTucController@getSua');
+	});
